5d1bf9fef4de66ba2d4c25dc8e672fafa06261fb
saad-mumtaz977 <saad.mumtaz@devsinc.com>
2023-11-06 17:12:50 +0500

    practicing properties in C#

---
+++ Basics/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class Employee
+    {
+
+        private int _id = 0;
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _id = value;
+                }
+                else
+                {
+                    _id = 9999;
+                }
+            }
+        }
+
+        private string _name = "";
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    _name = "";
+                }
+            }
+        }
+
+
+        public void show() {
+            Console.WriteLine("Id: " + Id);
+            Console.WriteLine("Name: " + Name);
+        }
+
+    }
+}
+
--- Basics/Program.cs
+++ Basics/Program.cs
@@ -8,31 +8,13 @@ namespace Basics
 {
     internal class Program
     {
-        public static void swap(ref int a, ref int b) {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
+     
         static void Main(string[] args)
         {
-            Calcualator c1 = new Calcualator();
-            Console.WriteLine(c1.addition(2, 6));
-            Console.WriteLine(c1.divide(4, 10));
-            Console.WriteLine(c1.multiply(10, 10));
-            Console.WriteLine(c1.subtract(10, 10));
-
-            int a = 10;
-            int b = 12;
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-
-            Console.WriteLine("swapping");
-            swap(ref a, ref b);
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-
+           Employee employee = new Employee();
+            employee.Id = 1;
+            employee.Name = "Test";
+            employee.show();
 
         }
     }
