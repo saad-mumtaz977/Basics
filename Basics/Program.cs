6d573fcd9803f325c625549161cb9ae5c2b403a1
saad-mumtaz977 <saad.mumtaz@devsinc.com>
2023-11-06 15:58:59 +0500

    practiced functions

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -10,64 +10,11 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++) {
-                if (i == 5)
-                {
-                    Console.WriteLine("Counter is at " + i++);
-                    continue;
-                }
-                else if (i > 5) {
-                    Console.WriteLine("Counter has reached above 5");
-                }
-
-                else
-                {
-                    Console.WriteLine("Counter has not reached 5 yet");
-                }
-            }
-
-
-            int y = 1;
-            while (y <= 10) { 
-                Console.WriteLine($"5 x {y} = {5*y}");
-                y++;
-            }
-
-            Console.WriteLine("Enter No between 1 - 5 (-1 to exit)");
-            string choice = Console.ReadLine();
-            if (choice == "-1") {
-                goto stmt9;
-            }
-
-            switch (choice) {
-
-                case "1":
-                    Console.WriteLine("you choose one");
-                    break;
-                case "2":
-                    Console.WriteLine("you choose two");
-                    break;
-                case "3":
-                    Console.WriteLine("you choose three");
-                    break;
-                case "4":
-                    Console.WriteLine("you choose four");
-                    break;
-                case "5":
-                    Console.WriteLine("you choose five");
-                    break;
-                default: 
-                    Console.WriteLine("Invalid choice");
-                    break;
-            }
-
-            int[] arr = { 1, 3, 4, 6, 7, 8 };
-            foreach (var v in arr) {
-                Console.WriteLine (v);
-            }
-
-        stmt9:
-            return;
+            Calcualator c1 = new Calcualator();
+            Console.WriteLine(c1.addition(2, 6));
+            Console.WriteLine(c1.divide(4, 10));
+            Console.WriteLine(c1.multiply(10, 10));
+            Console.WriteLine(c1.subtract(10, 10));
         }
     }
 }
