33b72e1c540b676cfbad1d5f67eb6ad728b4eaa4
saad-mumtaz977 <saad.mumtaz@devsinc.com>
2023-11-06 16:05:12 +0500

    practiced functions

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -8,6 +8,11 @@ namespace Basics
 {
     internal class Program
     {
+        public static void swap(ref int a, ref int b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
         static void Main(string[] args)
         {
             Calcualator c1 = new Calcualator();
@@ -15,6 +20,20 @@ namespace Basics
             Console.WriteLine(c1.divide(4, 10));
             Console.WriteLine(c1.multiply(10, 10));
             Console.WriteLine(c1.subtract(10, 10));
+
+            int a = 10;
+            int b = 12;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.WriteLine("swapping");
+            swap(ref a, ref b);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
         }
     }
 }
