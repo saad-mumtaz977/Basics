6155ae117c606879f6da26bf5c400ff564bfbb54
saad-mumtaz977 <saad.mumtaz@devsinc.com>
2023-11-06 15:14:57 +0500

    Covering datatypes, variables, typecasting etc

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -10,6 +10,7 @@ namespace Basics
     {
         static void Main(string[] args)
         {
+            /* 
             //console class is a static class 
             //properties of console class
             Console.BackgroundColor = ConsoleColor.White;
@@ -32,7 +33,49 @@ namespace Basics
             //readkey and read method
             Console.WriteLine("Enter a no to get its ASCII value");
             int z = Console.Read();
-            Console.WriteLine(z);
+            Console.WriteLine(z)
+            */
+
+
+
+            //DATA TYPES
+            /*
+            Console.WriteLine($"Byte => Minimum Range:{byte.MinValue} and Maximum Range:{byte.MaxValue}");
+            Console.WriteLine($"Integer => Minimum Range:{int.MinValue} and Maximum Range:{int.MaxValue}");
+            Console.WriteLine($"Float => Minimum Range:{float.MinValue} and Maximum Range:{float.MaxValue}");
+            Console.WriteLine($"Long => Minimum Range:{long.MinValue} and Maximum Range:{long.MaxValue}");
+            Console.WriteLine($"Double => Minimum Range:{double.MinValue} and Maximum Range:{double.MaxValue}");
+            Console.ReadKey();  */
+
+            string s1 = "saad mumtaz";
+            string s2 = s1;
+
+            //now s2 will point to a new string object on heap
+            s2 = "saad mumtaz khan";
+            Console.WriteLine(s1);
+
+
+            int[] arr = { 1, 2, 5, 6 };
+            int[] arr2 = arr;
+
+            arr2[1] = 3;
+
+            Console.WriteLine(arr[1]);
+
+            //type casting
+            int a = 12;
+            float f1 = a;  //implicit
+
+            float f2 = 3.8971f;
+            int b = (int) f2; //explicit
+
+
+            //through helper methods
+            string str1 = "43";
+            int c = Convert.ToInt32(str1);
+            Console.WriteLine(c);   
+
+
         }
     }
 }
